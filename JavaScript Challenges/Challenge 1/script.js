79708885343d9b529502ade1a9a2f71e75aad6ae
Silvana Kordab <silvanakordab2021@gmail.com>
2023-08-31 23:50:35 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -24,3 +24,57 @@ console.log(name+ "'s age: " + age);
 console.log("family name:", lastName);
 console.log("Date Of Birth:", DOB);
 
+// og the data types of the following values to the console:
+// "This is a text"
+// true
+// false
+// 1215
+// "999"
+// {}
+// []
+// null
+// undefined
+
+console.log("Data types:");
+console.log("Type of 'This is a text':", typeof "This is a text");
+console.log("Type of true:", typeof true);
+console.log("Type of false:", typeof false);
+console.log("Type of 1215:", typeof 1215);
+console.log("Type of '999':", typeof "999");
+console.log("Type of {}: ", typeof {});
+console.log("Type of []:", typeof []);
+console.log("Type of null:", typeof null);
+console.log("Type of undefined:", typeof undefined);
+
+// Declare 6 variables and assign them different data types.
+// Log each variable along with its data type.
+
+var number = 26;
+var string = "java script!";
+var boolean = true;
+var object = { key: "value" };
+var array = [5, 6, 8];
+var nullv = null;
+
+console.log("Variable data types:");
+console.log("Type of numberVar:", typeof number, "Value:", number);
+console.log("Type of stringVar:", typeof string, "Value:", string);
+console.log("Type of booleanVar:", typeof boolean, "Value:", boolean);
+console.log("Type of objectVar:", typeof object, "Value:", object);
+console.log("Type of arrayVar:", typeof array, "Value:", array);
+console.log("Type of nullVar:", typeof nullv, "Value:", nullv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
