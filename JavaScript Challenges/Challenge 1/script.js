971d73d3cee93559191e23029a0a6fca663d1a4d
Silvana Kordab <silvanakordab2021@gmail.com>
2023-08-31 23:37:46 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -5,3 +5,22 @@ console.log('I am linked now!');
 
 var name="Silvana";
 alert("My name is: " + name);
+
+// Declare 3 variables (age, lastName, DOB) using 3 different declaration methods.
+// Assign appropriate values to each variable.
+// Experiment with changing variable values.
+// Log all 3 variables to the console.
+// Compare the outcomes of the three declaration methods.
+// Commit: "Challenge 1 - Done with Step 3"
+
+var age=20;
+let lastName="Kordab";
+const DOB= "2003-06-5";//const cant be assignned l2na bdal constant w ma btet8yar 
+
+age=21;
+lastName="Abou Diab";
+
+console.log(name+ "'s age: " + age);
+console.log("family name:", lastName);
+console.log("Date Of Birth:", DOB);
+
