4a4c6795ac1547516d73cc14dfdfdc5aa70c3b62
Silvana Kordab <silvanakordab2021@gmail.com>
2023-08-31 23:13:31 +0300

    Challenge 1 - Done with Step 2

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,1 +1,7 @@
 console.log('I am linked now!');
+// Inside the JS file, declare a variable name.
+// Assign your name as the value for the name variable.
+// Display the value of the variable in an alert box.
+
+var name="Silvana";
+alert("My name is: " + name);
