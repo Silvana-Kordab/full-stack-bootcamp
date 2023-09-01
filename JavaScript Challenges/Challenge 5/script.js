f73717c4834e6b6713150a23e2157e454c120fb8
Silvana Kordab <silvanakordab2021@gmail.com>
2023-09-01 21:36:56 +0300

    Challenge 5.1 - Done with step 3

--- JavaScript Challenges/Challenge 5/script.js
+++ JavaScript Challenges/Challenge 5/script.js
@@ -61,3 +61,77 @@ document.addEventListener("keydown", function(event) {
       break;
   }
 });
+
+const form = document.createElement("form");
+
+const fullNameLabel = document.createElement("label");
+fullNameLabel.textContent = "Full Name:";
+const fullNameInput = document.createElement("input");
+fullNameInput.type = "text";
+fullNameInput.name = "fullName";
+fullNameInput.required = true;
+
+const emailLabel = document.createElement("label");
+emailLabel.textContent = "Email Address:";
+const emailInput = document.createElement("input");
+emailInput.type = "email";
+emailInput.name = "email";
+emailInput.required = true;
+
+const passwordLabel = document.createElement("label");
+passwordLabel.textContent = "Password:";
+const passwordInput = document.createElement("input");
+passwordInput.type = "password";
+passwordInput.name = "password";
+passwordInput.required = true;
+
+const confirmPasswordLabel = document.createElement("label");
+confirmPasswordLabel.textContent = "Confirm Password:";
+const confirmPasswordInput = document.createElement("input");
+confirmPasswordInput.type = "password";
+confirmPasswordInput.name = "confirmPassword";
+confirmPasswordInput.required = true;
+const submitButton = document.createElement("button");
+submitButton.type = "submit";
+submitButton.textContent = "Submit";
+form.appendChild(fullNameLabel);
+form.appendChild(fullNameInput);
+form.appendChild(document.createElement("br"));
+form.appendChild(emailLabel);
+form.appendChild(emailInput);
+form.appendChild(document.createElement("br"));
+form.appendChild(passwordLabel);
+form.appendChild(passwordInput);
+form.appendChild(document.createElement("br"));
+form.appendChild(confirmPasswordLabel);
+form.appendChild(confirmPasswordInput);
+form.appendChild(document.createElement("br"));
+form.appendChild(submitButton);
+document.body.appendChild(form);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
