951d581ac3c40d38e355c7cb5fe7ba2655b1176f
Silvana Kordab <silvanakordab2021@gmail.com>
2023-09-01 10:13:37 +0300

    Challenge 2 - Done with step 1

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,21 @@
+// Declare a variable age and assign your age as a value.
+// Using an if else statement, check whether the age is greater than or equal to 20.
+// If yes, log to the console: "You can enter this room."
+// If no, log to the console: "You can't enter this room."
+
+var age= 20;
+if (age >= 20)
+ {console.log("You can enter this room.")}
+else
+ console.log("You can't enter this room.");
+
+
+const expression1 = 5 > 3;
+const expression2 = 10 === '10';
+const expression3 = true && false;
+const expression4 = !expression3;
+
+console.log("Expression 1:", expression1);
+console.log("Expression 2:", expression2);
+console.log("Expression 3:", expression3);
+console.log("Expression 4:", expression4);
