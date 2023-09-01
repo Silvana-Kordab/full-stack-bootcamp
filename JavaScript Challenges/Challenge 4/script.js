6ef78407aa16ae3daacbdd6c516995162a0dbfc5
Silvana Kordab <silvanakordab2021@gmail.com>
2023-09-01 18:40:59 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -1,4 +1,8 @@
 const fiveNumbers = [1, 2, 3, 4, 5];
+const nextThreeNumbers = [6, 7, 8];
+const allNumbers = [...fiveNumbers, ...nextThreeNumbers];
+
+console.log(allNumbers); 
 
 const personalInfo = {
   name: "Silvana",
@@ -7,3 +11,10 @@ const personalInfo = {
   location: "Jahlieh Al Chouf",
   nationality: "Lebanese"
 };
+const additionalInfo ={
+    occupation: "Website Developer",
+    hobby: "unknown",
+    education:"undergradute CS student ",
+}
+const fullInfo = { ...personalInfo, ...additionalInfo };
+console.log(fullInfo);
